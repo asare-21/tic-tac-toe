b178996228ad59de7ad5ac4684053f387c5e7dbe
Joseph Asare <jasare4525@gmail.com>
2022-08-14 16:17:42 +0000

    testing new routes

--- routes/auth.js
+++ routes/auth.js
@@ -8,14 +8,14 @@ router.get('/check/:username', (req, res) => {
     const query = req.params.username
     try {
         UserModel.findOne({ username: query }, (err, user) => {
-            if (err) return res.statusCode(400).json({ success: false, error: err })
-            if (user) return res.statusCode(200).json({
+            if (err) return res.status(400).json({ success: false, error: err })
+            if (user) return res.status(200).json({
                 success: true, user: {
                     exists: true,
                     user: user._id
                 }
             })
-            return res.statusCode(200).json({
+            return res.status(200).json({
                 success: true, user: {
                     exists: false,
                     user: null
@@ -24,7 +24,7 @@ router.get('/check/:username', (req, res) => {
         })
     } catch (e) {
         console.log("Username check error: ", e)
-        return res.statusCode(400).json({ success: false, error: e })
+        return res.status(400).json({ success: false, error: e })
     }
 })
 
@@ -33,31 +33,31 @@ router.post("/login", (req, res) => {
     try {
         const { username, password } = req.body;
         UserModel.findOne({ username: username }, (err, user) => {
-            if (err) return res.statusCode(400).json({ success: false, error: err })
-            if (!user) return res.statusCode(200).json({ success: false, error: "User not found" })
+            if (err) return res.status(400).json({ success: false, error: err })
+            if (!user) return res.status(200).json({ success: false, error: "User not found" })
             // Compare password
             bcrypt.compare(password, hash, function (err, result) {
-                if (err) return res.statusCode(400).json({ success: false, error: err })
+                if (err) return res.status(400).json({ success: false, error: err })
                 if (!result) {
                     // Incorrect password
-                    return res.statusCode(200).json({ success: false, error: "Password incorrect" })
+                    return res.status(200).json({ success: false, error: "Password incorrect" })
                 }
             });
             // Password correct
-            return res.statusCode(200).json({ success: true, user: user })
+            return res.status(200).json({ success: true, user: user })
         })
     } catch (e) {
         console.log("Username authentication error: ", e)
-        return res.statusCode(400).json({ success: false, error: e })
+        return res.status(400).json({ success: false, error: e })
     }
 })
 
 router.post("/create", (req, res) => {
     try {
-        if (req.body.username === null || req.body.username === undefined) return res.statusCode(400).json({ success: false, error: "Username/Password is required" })
+        if (req.body.username === null || req.body.username === undefined) return res.status(400).json({ success: false, error: "Username/Password is required" })
         UserModel.findOne({ username: req.body.username }, (err, user) => {
-            if (err) return res.statusCode(400).json({ success: false, error: err })
-            if (user) return res.statusCode(200).json({ success: false, error: "Username already exists" })
+            if (err) return res.status(400).json({ success: false, error: err })
+            if (user) return res.status(200).json({ success: false, error: "Username already exists" })
             // Create user
             bcrypt.genSalt(10, function (err, salt) {
                 bcrypt.hash(req.body.password, salt, function (err, hash) {
@@ -68,12 +68,12 @@ router.post("/create", (req, res) => {
                     }).save()
                 });
             });
-            return res.statusCode(200).json({ success: true, })
+            return res.status(200).json({ success: true, })
 
         })
     } catch (e) {
         console.log("Username creation error: ", e)
-        return res.statusCode(400).json({ success: false, error: e })
+        return res.status(400).json({ success: false, error: e })
     }
 })
 module.exports.authRouter = router;
